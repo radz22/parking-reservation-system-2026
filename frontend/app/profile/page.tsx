a93852ed53e3f948bf968166340299b569b41cf2
radz22 <radzsantillan01@gmail.com>
2026-04-04 15:12:01 +0800

    chore: implement private page

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,11 +1,11 @@
-
-import { Profile } from "@/components/home/profile"; 
-
-export default function Page() {
-  return (
-    // 2. Balutin ang content ng Dashboard Wrapper
-   
-     <Profile></Profile>
-   
-  );
+import { Profile } from '@/components/home/profile';
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect('/sign-in');
+  }
+  return <Profile />;
 }
